db71c8cd82f61da9d557ca6c885e03b8d54dc746
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-11-19 16:39:51 +0530

    router added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import TextEditor from "./TextEditor";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import NavigateDocument from "./NavigateDocument";
 
 function App() {
   return (
-    <>
-       <TextEditor />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/documents/:id" element={<TextEditor />}></Route>
+        <Route path="/" exact element={<NavigateDocument />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
